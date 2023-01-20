12a713bb3f4a946d663e6b53521c1c5fd73c112c
jvhorcs0123 <jvhorca@gmail.com>
2023-01-20 17:14:21 +0800

    updatecommit

--- pages/universe/ExportPDF.php
+++ pages/universe/ExportPDF.php
@@ -18,20 +18,25 @@ $res = $record->pdfRecord($_GET['id']);
 
 $pdf = new FPDF();
 $pdf->AddPage();
-$pdf->SetFont('Arial','',16);
+$pdf->SetFont('Arial','',12);
 $pdf->Image('FPDF184/EMBLOGO.jpg', 10, 10, -180);
-$pdf->Ln(18);
+$pdf->Ln(30);
 
 
+$array = array($res);
+foreach($array as $value) {
+    // $pdf->Write(5,$value);
+    // var_dump($value);
+    $pdf->Write(3,'Report Control: ');
+    $pdf->Write(3,$value["Reportcontrol"]);
+    $pdf->Ln(5);
+    $pdf->Write(3,'Establishment Name: ');
+    $pdf->Write(3,$value["ProjectName"]);
 
-foreach($res as $value) {
-    $pdf->Write(5,$value);
+
 }
 
 $pdf->Output();
-
-
-
 
 ?>
 <!-- <script src="../../build/js/ajax.js"></script> -->
